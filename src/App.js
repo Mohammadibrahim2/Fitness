7154da03ded74e01b4eb7c659e783ed01ffcf567
Mohammad Ibrahim <mohammadibrahim6454@gmail.com>
2022-09-29 21:04:24 +0600

    finishing the card section

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header/Header';
+import Card from './Component/Card/CardSection'
+
+
+function App() {
+
+  return (
+    <div>
+      <Header></Header>
+      <Card></Card>
+      
+      
+    </div>
+  );
+}
+
+
+
+export default App;
